24e62ca34d56a1fdbf09cbac46fb7227aace7649
Angel Orellana <angelorellana.cvi@gmail.com>
2020-09-29 16:36:57 -0300

    Change to start creating static components

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
 
 function App() {
   return (<div>
     <h1>Ja<span className="highlight">mmm</span>ing</h1>
     <div className="App">
-      -- Add a SearchBar component --&gt;
+      <SearchBar />
       <div className="App-playlist">
-        -- Add a SearchResults component --&gt;
-        -- Add a Playlist component --&gt;
+        <SearchResults />
+        <Playlist />
       </div>
     </div>
   </div>
